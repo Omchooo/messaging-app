4762d405643cc888ef34eccb1471d61bb632a9ef
omcho <omersahmanovic@gmail.com>
2023-05-19 23:02:56 +0200

    chat overlapping bug fix

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{ $post->getFirstMediaUrl() }}" alt="Image" class="min-w-[26rem] max-w-md" />
             </picture>
             <div class="divider divider-horizontal mx-0 max-w-[0.125rem]"></div>
-            <div class=" w-[38rem] max-h-[37.333rem] flex flex-col">
+            <div class=" w-[38rem] flex flex-col">
                 <div class="flex items-center justify-between py-2 px-2 bg-base-300">
                     <div class="avatar items-center">
                         <div class="w-10 rounded-full">
@@ -34,90 +34,88 @@
                     @endif
                 </div>
                 {{-- <div class="divider my-0"></div> --}}
-                <div class="mx-3 my-2 flex flex-col gap-6 min-h-[20rem] h-full max-h-[21.75rem] overflow-y-auto">
-                    @isset($post->desc)
-                        <div>
-                            <div class="avatar items-center">
-                                {{-- <div class="w-10 rounded-full">
-                                    @if ($post->user->getFirstMediaUrl('profile', 'avatar'))
-                                        <img src="{{ $post->user->getFirstMediaUrl('profile', 'avatar') }}" />
-                                    @else
-                                        <img src="https://placeimg.com/192/192/people" />
-                                    @endif
-                                </div>
-                                <span class="mx-2 truncate max-w-[7.5rem]">{{ $post->user->username }} - </span> --}}
-                                <span class="break-all max-w-[24rem] text-lg">{{ $post->desc }}</span>
-                            </div>
-                        </div>
-                    @endisset
 
-                    {{-- comment with replies --}}
-                    @isset($comments)
-                        @foreach ($comments as $comment)
-                            <div class="flex flex-col gap-4">
-                                {{-- comment by user --}}
-                                <div class="flex justify-between items-center">
-                                    <div class="h-full flex items-center">
-                                        <div class="avatar">
-                                            <div class="w-8 rounded-full">
-                                                @if ($comment->user->getFirstMediaUrl('profile', 'avatar'))
-                                                    <img src="{{ $comment->user->getFirstMediaUrl('profile', 'avatar') }}" />
-                                                @else
-                                                    <img src="https://placeimg.com/192/192/people" />
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="max-w-[30rem] mx-4 flex flex-col w-full items-start">
-                                        <p class="w-full"><a
-                                                href="{{ route('viewprofile.index', $comment->user) }}">{{ $comment->user->username }}</a>
-                                        </p>
-                                        <span class="break-words text-md">{{ $comment->comment }}</span>
-                                        <span class="text-xxs my-1">{{ $comment->created_at->diffForHumans() }}</span>
-                                    </div>
-                                    <livewire:likes :comment="$comment">
-                                </div>
-
-                                {{-- load more replies --}}
-                                {{-- <button class="btn btn-xs btn-outline border-0 ml-10 w-24">View replies</button> --}}
-
-                                {{-- <a href="" class="ml-12 text-sm hover:opacity-80">View replies</a> --}}
-
-                                {{-- replies --}}
-                                {{-- <div class="flex justify-between items-center">
-                                    <div class="h-full">
-                                        <div class="avatar items-center ml-14">
-                                            <div class="w-10 rounded-full">
+                <div class="flex flex-col relative h-full items-center my-1">
+                    <div class=" flex flex-col gap-6 min-h-[20rem] h-full w-[calc(100%-0.5rem)] overflow-y-auto absolute">
+                        @isset($post->desc)
+                            <div>
+                                <div class="avatar items-center">
+                                    {{-- <div class="w-10 rounded-full">
+                                            @if ($post->user->getFirstMediaUrl('profile', 'avatar'))
+                                                <img src="{{ $post->user->getFirstMediaUrl('profile', 'avatar') }}" />
+                                            @else
                                                 <img src="https://placeimg.com/192/192/people" />
+                                            @endif
+                                        </div>
+                                        <span class="mx-2 truncate max-w-[7.5rem]">{{ $post->user->username }} - </span> --}}
+                                    <span class="break-all max-w-[24rem] text-lg">{{ $post->desc }}</span>
+                                </div>
+                            </div>
+                        @endisset
+                        {{-- comment with replies --}}
+                        @isset($comments)
+                            @foreach ($comments as $comment)
+                                <div class="flex flex-col gap-4">
+                                    {{-- comment by user --}}
+                                    <div class="flex justify-between items-center">
+                                        <div class="h-full flex items-center">
+                                            <div class="avatar">
+                                                <div class="w-8 rounded-full">
+                                                    @if ($comment->user->getFirstMediaUrl('profile', 'avatar'))
+                                                        <img src="{{ $comment->user->getFirstMediaUrl('profile', 'avatar') }}" />
+                                                    @else
+                                                        <img src="https://placeimg.com/192/192/people" />
+                                                    @endif
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="max-w-[26rem] mx-4 flex flex-col w-full items-start">
-                                        <p class=" w-full">username - </p>
-                                        <span class="break-words text-sm">reply</span>
-                                    </div>
-                                    <div>
-                                        <div
-                                            class="btn btn-circle btn-sm btn-link flex justify-center items-center float-right mr-1">
-                                            <svg height="12px" width="12px" viewBox="0 0 24 24" fill="#8e8e8e"
-                                                color="#8e8e8e  #ed4956">
-                                                <path
-                                                    d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z">
-                                                </path>
-                                            </svg>
+                                        <div class="max-w-[30rem] mx-4 flex flex-col w-full items-start">
+                                            <p class="w-full"><a
+                                                    href="{{ route('viewprofile.index', $comment->user) }}">{{ $comment->user->username }}</a>
+                                            </p>
+                                            <span class="break-words text-md">{{ $comment->comment }}</span>
+                                            <span class="text-xxs my-1">{{ $comment->created_at->diffForHumans() }}</span>
                                         </div>
+                                        <livewire:likes :comment="$comment">
                                     </div>
-                                </div> --}}
-
+                                    {{-- load more replies --}}
+                                    {{-- <button class="btn btn-xs btn-outline border-0 ml-10 w-24">View replies</button> --}}
+                                    {{-- <a href="" class="ml-12 text-sm hover:opacity-80">View replies</a> --}}
+                                    {{-- replies --}}
+                                    {{-- <div class="flex justify-between items-center">
+                                            <div class="h-full">
+                                                <div class="avatar items-center ml-14">
+                                                    <div class="w-10 rounded-full">
+                                                        <img src="https://placeimg.com/192/192/people" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="max-w-[26rem] mx-4 flex flex-col w-full items-start">
+                                                <p class=" w-full">username - </p>
+                                                <span class="break-words text-sm">reply</span>
+                                            </div>
+                                            <div>
+                                                <div
+                                                    class="btn btn-circle btn-sm btn-link flex justify-center items-center float-right mr-1">
+                                                    <svg height="12px" width="12px" viewBox="0 0 24 24" fill="#8e8e8e"
+                                                        color="#8e8e8e  #ed4956">
+                                                        <path
+                                                            d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z">
+                                                        </path>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                        </div> --}}
+                                </div>
+                            @endforeach
+                        @endisset
+                        {{-- load more comments --}}
+                        @if ($comments->hasMorePages())
+                            <div class="flex w-full justify-center my-4">
+                                <button class="btn btn-xs {{-- loading --}} w-32">Load more</button>
                             </div>
-                        @endforeach
-                    @endisset
-                    {{-- load more comments --}}
-                    @if ($comments->hasMorePages())
-                        <div class="flex w-full justify-center my-4">
-                            <button class="btn btn-xs {{-- loading --}} w-32">Load more</button>
-                        </div>
-                    @endif
+                        @endif
+                    </div>
                 </div>
                 <div class="divider my-0"></div>
 
