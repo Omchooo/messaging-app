4473057edfe71786830ff5ad55bbf0902b398d24
omcho <omersahmanovic@gmail.com>
2023-06-01 23:12:07 +0200

    added cascade on delete to chat-user

--- database/migrations/2023_05_09_134344_create_chat_user_table.php
+++ database/migrations/2023_05_09_134344_create_chat_user_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('chat_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('chat_id')->constrained();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('chat_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
