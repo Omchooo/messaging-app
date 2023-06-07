30e6a2a12779563ed9cdb2ef3f40a4e79f8089ba
omcho <omersahmanovic@gmail.com>
2023-06-07 12:26:39 +0200

    dynamic title for edit page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.main')
 
+@section('title', 'Settings • InstaByte')
+
 @section('content')
     <div class="container mx-auto max-w-5xl my-5 mt-0 flex justify-center">
         <livewire:drawer :user="$user">
